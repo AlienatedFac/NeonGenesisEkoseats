c42878f33acd868832638962d796989fcf7a167f
Fernando Araujo <oppaiskoibito@gmail.com>
2019-07-11 16:29:15 -0500

    Update catalogo.php

--- catalogo.php
+++ catalogo.php
@@ -180,23 +180,6 @@
         <div class="android-more-section">
           <div class="android-section-title mdl-typography--display-1-color-contrast"><?php echo $nombre_seccion; ?></div>
           <div class="android-card-container mdl-grid">
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
-              <div class="mdl-card__media">
-                <img src="images/more-from-1.png">
-              </div>
-              <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Producto</h4>
-              </div>
-              <div class="mdl-card__supporting-text">
-                <span class="mdl-typography--font-light mdl-typography--subhead">Descripcion</span>
-              </div>
-              <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
-                    Carrito, ver mas
-                   <i class="material-icons">chevron_right</i>
-                 </a>
-              </div>
-            </div>
               <?php
                   $conexion = mysqli_connect("localhost", "root", "", "ekoseat_bdd");
                     for($contador=1;$contador<5;$contador++){
@@ -240,25 +223,7 @@
                         } 
                   }      
               
-                ?>
-
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
-              <div class="mdl-card__media">
-                <img src="images/more-from-4.png">
-              </div>
-              <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Producto</h4>
-              </div>
-              <div class="mdl-card__supporting-text">
-                <span class="mdl-typography--font-light mdl-typography--subhead">Descripcion</span>
-              </div>
-              <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="https://androidify.com" target="_blank">
-                   Carrito, ver mas
-                   <i class="material-icons">chevron_right</i>
-                 </a>
-              </div>
-            </div>       
+                ?>     
           </div>
         </div>
 
