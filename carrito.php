a27af7ce4c25edf79cd7694755b7507d390da412
Sergio1310 <40905681+Sergio1310@users.noreply.github.com>
2019-07-22 08:17:53 -0500

    boton del carrito

--- carrito.php
+++ carrito.php
@@ -155,17 +155,17 @@
    
               if(!isset($_SESSION['id_usuario']) || !isset($_SESSION['nombre_usuario'])){
               ?>
-           <div class="android-customized-section">
-          <div class="android-customized-section-text">
-            <div class="mdl-typography--font-light mdl-typography--display-1-color-contrast">UPS!</div>
-            <p class="mdl-typography--font-light">
-              Parece que no has iniciado sesion :c, para agregar cosas y ver tu carrito 
-              <br>
-              <a href="login.php" class="android-link mdl-typography--font-light">inicia sesion aqui!.</a>
-            </p>
-          </div>
-          <div class="android-customized-section-image"></div>
-        </div>
+                 <div class="android-customized-section">
+                <div class="android-customized-section-text">
+                  <div class="mdl-typography--font-light mdl-typography--display-1-color-contrast">UPS!</div>
+                  <p class="mdl-typography--font-light">
+                    Parece que no has iniciado sesion :c, para agregar cosas y ver tu carrito 
+                    <br>
+                    <a href="login.php" class="android-link mdl-typography--font-light">inicia sesion aqui!.</a>
+                  </p>
+                </div>
+                <div class="android-customized-section-image"></div>
+              </div>
               <?php
               }else{
                   
@@ -200,51 +200,50 @@
                         </div>
             
                   <?php
-                    $total=$total+$subtotal;
-                    
-            
-              }
+                    $total=$total+$subtotal;  
+                  }
                   if($total>=2500)
                     {
                         $envio_todo=0;
                     } else{
                         $envio_todo=200;
-                    }            
-                    }
-                  ?>
-             <div class="android-customized-section-text">
-              <div  align="center" class="mdl-typography--font-light mdl-typography--display-1-color-contrast">Comprar Todo! :D</div>
-              <p class="mdl-typography--font-light">
-                  <!--
-                Ponga las cosas que usted desee a la derecha en la pantalla principal: las últimas noticias, el clima o una galeria con sus fotos recientes.
-                <br>
-                <a href="" class="android-link mdl-typography--font-light">Busca productos de interes.</a>
--->
-              </p>
-             <div align="center" class="Comprar">
-              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-              <input type="hidden" name="cmd" value="_xclick">
-              <input type="hidden" name="business" value="substratum21@gmail.com">
-              <input type="hidden" name="lc" value="MX">
-              <input type="hidden" name="item_name" value="<?php Carrito ?>">
-              <input type="hidden" name="item_number" value="Id">
-              <input type="hidden" name="amount" value="<?php echo $total ?>.00">
-              <input type="hidden" name="currency_code" value="MXN">
-              <input type="hidden" name="button_subtype" value="services">
-              <input type="hidden" name="no_note" value="0">
-              <input type="hidden" name="tax_rate" value="0.000">
-              <input type="hidden" name="shipping" value="<?php echo $envio_todo ?>">
-              <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-              <input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-              <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-            </form>   
-                 
-                 </div>
-                
-                 
-            </div>
-               
-          
+                    }?>
+                    
+                  <div class="android-customized-section-text">
+                    <div  align="center" class="mdl-typography--font-light mdl-typography--display-1-color-contrast">Comprar Todo! :D</div>
+                    <p class="mdl-typography--font-light">
+                        <!--
+                      Ponga las cosas que usted desee a la derecha en la pantalla principal: las últimas noticias, el clima o una galeria con sus fotos recientes.
+                      <br>
+                      <a href="" class="android-link mdl-typography--font-light">Busca productos de interes.</a>
+      -->
+                    </p>
+                   <div align="center" class="Comprar">
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="business" value="substratum21@gmail.com">
+                    <input type="hidden" name="lc" value="MX">
+                    <input type="hidden" name="item_name" value="<?php Carrito ?>">
+                    <input type="hidden" name="item_number" value="Id">
+                    <input type="hidden" name="amount" value="<?php echo $total ?>.00">
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="hidden" name="button_subtype" value="services">
+                    <input type="hidden" name="no_note" value="0">
+                    <input type="hidden" name="tax_rate" value="0.000">
+                    <input type="hidden" name="shipping" value="<?php echo $envio_todo ?>">
+                    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+                    <input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                  </form>   
+                       
+                       </div>
+                      
+                       
+                  </div>
+                  <?php
+                  }
+                  
+                ?>
         </div>
 
         <footer class="android-footer mdl-mega-footer">
