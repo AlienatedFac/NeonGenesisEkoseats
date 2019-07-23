28bb17757e5c81b92b59ddb95729092fea00327d
Fernando Araujo <oppaiskoibito@gmail.com>
2019-07-22 21:54:33 -0500

    Boton con regreso

--- carrito.php
+++ carrito.php
@@ -229,22 +229,26 @@
       -->
                     </p>
                    <div align="center" class="Comprar">
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_xclick">
-                    <input type="hidden" name="business" value="substratum21@gmail.com">
-                    <input type="hidden" name="lc" value="MX">
-                    <input type="hidden" name="item_name" value="<?php Carrito ?>">
-                    <input type="hidden" name="item_number" value="Id">
-                    <input type="hidden" name="amount" value="<?php echo $total ?>.00">
-                    <input type="hidden" name="currency_code" value="MXN">
-                    <input type="hidden" name="button_subtype" value="services">
-                    <input type="hidden" name="no_note" value="0">
-                    <input type="hidden" name="tax_rate" value="0.000">
-                    <input type="hidden" name="shipping" value="<?php echo $envio_todo ?>">
-                    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-                    <input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-                  </form>   
+                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="oppaiskoibito@gmail.com">
+<input type="hidden" name="lc" value="MX">
+<input type="hidden" name="item_name" value="Carrito">
+<input type="hidden" name="item_number" value="Id">
+<input type="hidden" name="amount" value="<?php echo $total ?>.00">
+<input type="hidden" name="currency_code" value="MXN">
+<input type="hidden" name="button_subtype" value="services">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="undefined_quantity" value="1">
+<input type="hidden" name="rm" value="1">
+<input type="hidden" name="return" value="http://127.0.0.1/NeonGenesisEkoseats/sucess.php">
+<input type="hidden" name="cancel_return" value="http://127.0.0.1/NeonGenesisEkoseats/cancel.php">
+<input type="hidden" name="shipping" value="<?php echo $envio_todo ?>">
+<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
+<input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+</form>   
                        
                        </div>
                       
--- detalles.php
+++ detalles.php
@@ -200,22 +200,26 @@ $resultado = mysqli_fetch_array($consulta);
                   
                 
               </p>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-              <input type="hidden" name="cmd" value="_xclick">
-              <input type="hidden" name="business" value="substratum21@gmail.com">
-              <input type="hidden" name="lc" value="MX">
-              <input type="hidden" name="item_name" value="<?php echo $resultado['nombre_producto'] ?>">
-              <input type="hidden" name="item_number" value="Id">
-              <input type="hidden" name="amount" value="<?php echo $resultado['precio_producto'] ?>.00">
-              <input type="hidden" name="currency_code" value="MXN">
-              <input type="hidden" name="button_subtype" value="services">
-              <input type="hidden" name="no_note" value="0">
-              <input type="hidden" name="tax_rate" value="0.000">
-              <input type="hidden" name="shipping" value="<?php echo $envio ?>">
-              <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-              <input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-              <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-            </form>
+           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="oppaiskoibito@gmail.com">
+<input type="hidden" name="lc" value="MX">
+<input type="hidden" name="item_name" value="<?php echo $resultado['nombre_producto'] ?>">
+<input type="hidden" name="item_number" value="<?php echo $resultado['id_producto'] ?>">
+<input type="hidden" name="amount" value="<?php echo $resultado['precio_producto'] ?>.00">
+<input type="hidden" name="currency_code" value="MXN">
+<input type="hidden" name="button_subtype" value="services">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="undefined_quantity" value="1">
+<input type="hidden" name="rm" value="1">
+<input type="hidden" name="return" value="http://127.0.0.1/NeonGenesisEkoseats/sucess.php">
+<input type="hidden" name="cancel_return" value="http://127.0.0.1/NeonGenesisEkoseats/cancel.php">
+<input type="hidden" name="shipping" value="<?php echo $envio ?>">
+<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
+<input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+</form>
             </div>
           </div>
         </div>
